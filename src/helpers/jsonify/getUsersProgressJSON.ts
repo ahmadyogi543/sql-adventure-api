467b3c6d0ee95ed105716d2981545f266a4bad50
ahmadyogi543 <ahmadyogi543@gmail.com>
2024-08-31 23:40:42 +0800

    fix: not showing if not exists

--- src/helpers/jsonify/getUsersProgressJSON.ts
+++ src/helpers/jsonify/getUsersProgressJSON.ts
@@ -17,45 +17,40 @@ export function getUsersProgressJSON(input: any[]) {
       json.push(user);
     }
 
-    if (row.users_progress_id !== undefined) {
-      let progress = user.values.find(
-        (v) => v.stage_id === row.users_progress_stage_id
-      );
-
-      if (!progress) {
-        progress = {
-          stage_id: row.users_progress_stage_id,
-          no_of_missions: row.users_progress_no_of_missions,
-          last_attempted: formatDateToTimestamp(
-            new Date(row.users_progress_last_attempted)
-          ),
-          missions_attempted: [],
-        };
-        user.values.push(progress);
-      }
-
-      if (row.attempted_missions_id !== undefined) {
-        let mission = progress.missions_attempted.find(
-          (m) => m.mission_id === row.attempted_missions_mission_id
-        );
-
-        if (!mission) {
-          mission = {
-            mission_id: row.attempted_missions_mission_id,
-            attempt: row.attempted_missions_attempt,
-            last_attempted: formatDateToTimestamp(
-              new Date(row.attempted_missions_last_attempted)
-            ),
-            scores: [],
-          };
-          progress.missions_attempted.push(mission);
-        }
-
-        if (row.attempted_mission_scores_id !== undefined) {
-          mission.scores.push(row.attempted_mission_scores_score);
-        }
-      }
+    if (!row.users_progress_id) return;
+    let progress = user.values.find(
+      (v) => v.stage_id === row.users_progress_stage_id
+    );
+    if (!progress) {
+      progress = {
+        stage_id: row.users_progress_stage_id,
+        no_of_missions: row.users_progress_no_of_missions,
+        last_attempted: formatDateToTimestamp(
+          new Date(row.users_progress_last_attempted)
+        ),
+        missions_attempted: [],
+      };
+      user.values.push(progress);
     }
+
+    if (!row.attempted_missions_id) return;
+    let mission = progress.missions_attempted.find(
+      (m) => m.mission_id === row.attempted_missions_mission_id
+    );
+    if (!mission) {
+      mission = {
+        mission_id: row.attempted_missions_mission_id,
+        attempt: row.attempted_missions_attempt,
+        last_attempted: formatDateToTimestamp(
+          new Date(row.attempted_missions_last_attempted)
+        ),
+        scores: [],
+      };
+      progress.missions_attempted.push(mission);
+    }
+
+    if (!row.attempted_mission_scores_id) return;
+    mission.scores.push(row.attempted_mission_scores_score);
   });
 
   return json;
