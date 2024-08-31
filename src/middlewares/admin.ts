5d93716e28622efd5d1acff934ac7928e6c242f0
ahmadyogi543 <ahmadyogi543@gmail.com>
2024-08-31 08:58:09 +0800

    update: move import so that a-z

--- src/middlewares/admin.ts
+++ src/middlewares/admin.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 
-import { UserPayload } from "./types";
 import { sendForbiddenJSON, sendInternalServerErrorJSON } from "@/helpers";
+import { UserPayload } from "./types";
 
 interface AdminRequest extends Request {
   user?: UserPayload;
