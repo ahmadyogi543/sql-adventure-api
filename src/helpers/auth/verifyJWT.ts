0d4fb3a4c1e5a2afafbd6d20e466eff0e3b707fe
ahmadyogi543 <ahmadyogi543@gmail.com>
2024-09-01 07:52:55 +0800

    fix: wrong import

--- src/helpers/auth/verifyJWT.ts
+++ src/helpers/auth/verifyJWT.ts
@@ -1,7 +1,7 @@
 import { verify } from "jsonwebtoken";
 
 import { config } from "@/config";
-import { UserPayload } from "@/middlewares/authenticate";
+import { UserPayload } from "@/middlewares/types";
 
 export function verifyJWT(
   token: string,
