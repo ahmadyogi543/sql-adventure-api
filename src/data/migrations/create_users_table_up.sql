48901c1a595938857827e675330d4fe131a198e4
ahmadyogi543 <ahmadyogi543@gmail.com>
2024-08-31 09:08:28 +0800

    update: add default admin account

--- src/data/migrations/create_users_table_up.sql
+++ src/data/migrations/create_users_table_up.sql
@@ -20,3 +20,9 @@ CREATE TABLE IF NOT EXISTS banned_tokens (
   token TEXT NOT NULL,
   expires_at TEXT NOT NULL
 );
+
+-- one account to rule them all (i.e admin)
+-- username: admin
+-- password: 12345678
+INSERT OR IGNORE INTO users (id, username, password_hash, role)
+VALUES(1, 'admin', '$2a$12$LjDaqRkKlRRWWXzsGZJxFOmcdufcy8CB4fsevG9Hkb6zatDT0RyeK', 'admin');
